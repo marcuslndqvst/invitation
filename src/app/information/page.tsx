34fb422f3fa8e26a9c7b684ee3f7aecda077b9a6
marcuslndqvst <marcus.lindqvist@sr.se>
2024-02-28 22:41:06 +0100

    fix

--- src/app/information/page.tsx
+++ src/app/information/page.tsx
@@ -6,23 +6,24 @@ import ".././globals.css";
 
 export default function Home() {
   return (
-    <div className="bg-black flex items-center flex-col">
-      <Link href="/">
-        <div className="w-full flex justify-end p-5">X</div>
-      </Link>
-      <div className="flex flex-col p-24">
-        <h1 className="font-mono">Rymdkalas</h1>
-        <h2 className="font-mono">var?</h2>
-        <p className="font-mono">stövelvägen 12, solberga</p>
-        <h2 className="font-mono">när?</h2>
-        <p className="font-mono">16 mars 13.30</p>
+    <div className="bg-black flex flex-col items-center min-h-screen justify-between">
+      <div className="flex flex-row w-full justify-end">
+        <Link href="/">
+          <div className="w-full flex justify-end p-5">X</div>
+        </Link>
+      </div>
+      <div className="flex flex-col p-24 gap-3">
+        <h1 className="font-mono">välkommen på Edens & Umas rymdkalas</h1>
+        <p className="font-mono">på stövelvägen 12, solberga</p>
+        <p className="font-mono">16 mars 13.30-16.00</p>
+        <p className="font-mono">vi bjuder på fika!</p>
       </div>
       <Image
-        priority
-        height="300"
-        width="600"
-        src={astronaut}
-        alt="astronaut"
+        width={500}
+        height={500}
+        src="https://apod.nasa.gov/apod/image/2211/Butterfly_HubbleOstling_3656.jpg"
+        alt={""}
+        className="w-full"
       />
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 import Link from "next/link";
 import "./globals.css";
-import space from "../../public/space-galaxy.webp";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 background-image-space">
+    <main className="flex flex-col items-center justify-evenly min-h-screen background-image-space">
       <Video />
-      <Link href="/information">
+      <Link href="/information" className="animate-pulse">
         <div className="relative group cursor-pointer">
           <div className="absolute -inset-1 bg-gradient-to-r from-red-600 to-violet-600 rounded-full blur opacity-50 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
           <div className="relative px-20 py-6 bg-transparent ring-1 ring-gray-900/5 rounded-full leading-none flex items-top justify-start space-x-6">
             <div className="space-y-2">
-              <p className="text-yellow font-mono">Information</p>
+              <p className="text-yellow font-mono">info</p>
             </div>
           </div>
         </div>
@@ -22,8 +21,8 @@ export default function Home() {
 
 function Video() {
   return (
-    <video width="350" height="500" controls loop>
-      <source src="/movie.mov" type="video/mp4" />
+    <video height={500} width={320} controls loop>
+      <source src="/rymdkalas.mov" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   );
